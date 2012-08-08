673b25a33fe52b29280f6ed704b8866edf297c59
Michael Hiller <hiller@mpi-cbg.de>
2012-08-08 17:16:02 +0200

    added coloring of Bej and HL tracks back -- got lost during the massive merge conflicts

--- src/hg/hgTracks/hgTracks.c
+++ src/hg/hgTracks/hgTracks.c
@@ -4903,7 +4903,12 @@ if (!hideControls)
                     freeMem(url);
                     freeMem(longLabel);
                     }
-		hPrintf(" %s", track->shortLabel);
+	        if (startsWith("Bej ", track->shortLabel))
+                    hPrintf(" <font color=red>Bej </font>%s", track->shortLabel + 4);
+	        else if (startsWith("HL ", track->shortLabel))
+                    hPrintf(" <font color=red>HL </font>%s", track->shortLabel + 3);
+                else
+                    hPrintf(" %s", track->shortLabel);
 		if (tdbIsSuper(track->tdb))
 		    hPrintf("...");
 		hPrintf("<BR> ");
